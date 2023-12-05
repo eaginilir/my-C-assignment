9a58406ba8cc28ff487882c1757d50f697e646f1
eaginilir <147464632+eaginilir@users.noreply.github.com>
2023-12-05 17:21:44 +0800

    Add files via upload

---
+++ 矩阵旋转.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, i, j, cishu = 0;
+	char m[101][101];
+	cin >> n;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			cin >> m[i][j];
+		}
+	}
+	for (i = 0; i < (n+1)/2; i ++ )
+	{
+		for (j = 0; j < n/2; j++)
+		{
+			vector<char> values = { m[i][j],m[j][n - i - 1],m[n - 1 - i][n - 1 - j],m[n - 1 - j][i] };
+			int ones = count(values.begin(), values.end(), '1');
+			int zeros = 4 - ones;
+			cishu = cishu + min(ones, zeros);
+		}
+	}
+	if (n % 2 == 1)
+	{
+		cishu = cishu + ((m[n / 2][n / 2] == 1) ? 1: 0);
+	}
+	printf("%d", cishu);
+	return 0;
+}
