395da4f2d785e12f4bdd82e34c2636fb4aa59d8b
eaginilir <147464632+eaginilir@users.noreply.github.com>
2023-12-23 22:04:57 +0800

    Add files via upload

---
+++ 寻找分数最高的学生（不使用STL）/寻找分数最高的学生（不使用STL）.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+void swap(struct student& x, struct student& y);
+struct student
+{
+	int socre;
+	char name[20];
+}stu[100];
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> stu[i].socre >> stu[i].name;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i; j++)
+		{
+			if (stu[j].socre < stu[j + 1].socre)
+			{
+				swap(stu[j], stu[j + 1]);
+			}
+		}
+	}
+	cout << stu[0].name << endl;
+	return 0;
+}
+void swap(struct student& x, struct student& y)
+{
+	struct student temp = x;
+	x = y;
+	y = temp;
+}
